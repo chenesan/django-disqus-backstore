d19feac2bd42e2f58e90100f81f8b0b3baa3fc2c
chenesan <pipio1994@gmail.com>
2016-04-30 12:03:38 +0800

    Extract DisqusQuerySet in query.py
    Since most of the methods in ThreadQuerySet and PostQuerySet
    work in the same way(ex. `all`, `count`, `__getitem__`), We extract
    the common parts as DisqusQuerySet, and conserve the `get`, `filter`,
    `create` methods in Post/ThreadQuerySet.

--- disqus_backstore/query.py
+++ disqus_backstore/query.py
@@ -3,8 +3,7 @@ import copy
 from .disqus_interface import DisqusQuery
 
 
-class ThreadQuerySet(object):
-    # order_by, filter, query are required in admin.options.get_queryset
+class DisqusQuerySet(object):
     def __init__(self, model=None, query=None, using=None, hints=None):
         self.model = model
         self.query = query or DisqusQuery()
@@ -21,14 +20,45 @@ class ThreadQuerySet(object):
     def order_by(self, *args, **kwargs):
         return self
 
+    def complex_filter(self, filter_obj):
+        return self
+
+    def all(self, *args, **kwargs):
+        return self._clone()
+
+    def count(self, *args, **kwargs):
+        return len(self.data)
+
+    def iterator(self):
+        return iter(self.data)
+
+    def using(self, alias):
+        clone = self._clone()
+        clone._db = alias
+        return clone
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, i):
+        return self.data[i]
+
+    def _clone(self, **kwargs):
+        clone = self.__class__()
+        clone.rawdata = copy.deepcopy(self.rawdata)
+        clone.data = copy.deepcopy(self.data)
+        return clone
+
+
+class ThreadQuerySet(DisqusQuerySet):
     def get(self, *args, **kwargs):
         kwargs['thread'] = kwargs.pop('id')
         rawdata = self.query.get_threads_list(**kwargs)['response']
         if len(rawdata) > 2:
             raise self.model.MultipleObjectsReturned(
-            "get() returned more than one %s -- it returned %s!" %
-            (self.model._meta.object_name, len(rawdata))
-        )
+                "get() returned more than one %s -- it returned %s!" %
+                (self.model._meta.object_name, len(rawdata))
+            )
         elif len(rawdata) == 1:
             thread = rawdata[0]
             return self.create(
@@ -40,9 +70,6 @@ class ThreadQuerySet(object):
                 is_closed=thread.get('isClosed'),
                 is_spam=thread.get('isSpam'),
             )
-
-    def complex_filter(self, filter_obj):
-        return self
 
     def filter(self, *args, **kwargs):
         if not getattr(self, 'rawdata', None):
@@ -58,50 +85,8 @@ class ThreadQuerySet(object):
         ) for thread in self.rawdata['response']]
         return self
 
-    def all(self, *args, **kwargs):
-        return self._clone()
 
-    def count(self, *args, **kwargs):
-        return len(self.data)
-
-    def iterator(self):
-        return iter(self.data)
-
-    def using(self, alias):
-        clone = self._clone()
-        clone._db = alias
-        return clone
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, i):
-        return self.data[i]
-
-    def _clone(self, **kwargs):
-        clone = ThreadQuerySet()
-        clone.rawdata = copy.deepcopy(self.rawdata)
-        clone.data = copy.deepcopy(self.data)
-        return clone
-
-
-class PostQuerySet(object):
-    def __init__(self, model=None, query=None, using=None, hints=None):
-        self.model = model
-        self.query = query or DisqusQuery()
-        if using:
-            self.using = using
-        self.rawdata = []
-        self.data = []
-        self._prefetch_related_lookups = []
-
-    def create(self, *args, **kwargs):
-        obj = self.model(**kwargs)
-        return obj
-
-    def order_by(self, *args, **kwargs):
-        return self
-
+class PostQuerySet(DisqusQuerySet):
     def get(self, *args, **kwargs):
         """
         Because there's no "post" argument in diqus fourm/listPost API
@@ -110,7 +95,8 @@ class PostQuerySet(object):
         """
         post_id = kwargs.pop('id')
         post = self.query.get_post(post_id, **kwargs)['response']
-        thread = self.model._meta.get_field('thread').remote_field.model.objects.get(id=post['thread'])
+        thread_field = self.model._meta.get_field('thread')
+        thread = thread_field.remote_field.model.objects.get(id=post['thread'])
         return self.create(
             id=int(post.get('id')),
             forum=post.get('forum'),
@@ -121,14 +107,12 @@ class PostQuerySet(object):
             thread=thread,
         )
 
-    def iterator(self):
-        return iter(self.data)
-
     def filter(self, *args, **kwargs):
         if not getattr(self, 'rawdata', None):
             self.rawdata = self.query.get_posts_list()
         for post in self.rawdata['response']:
-            thread = self.model._meta.get_field('thread').remote_field.model.objects.get(id=post['thread'])
+            thread_field = self.model._meta.get_field('thread')
+            thread = thread_field.remote_field.model.objects.get(id=post['thread'])
             obj = self.create(
                 id=int(post.get('id')),
                 forum=post.get('forum'),
@@ -140,26 +124,3 @@ class PostQuerySet(object):
             )
             self.data.append(obj)
         return self
-
-    def all(self, *args, **kwargs):
-        return self._clone()
-
-    def count(self, *args, **kwargs):
-        return len(self.data)
-
-    def using(self, alias):
-        clone = self._clone()
-        clone._db = alias
-        return clone
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, i):
-        return self.data[i]
-
-    def _clone(self, **kwargs):
-        clone = PostQuerySet()
-        clone.rawdata = copy.deepcopy(self.rawdata)
-        clone.data = copy.deepcopy(self.data)
-        return clone
