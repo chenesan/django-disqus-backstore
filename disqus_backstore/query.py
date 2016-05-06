5d8a82e27949f3ec43508a8561d6ad792230a8c6
chenesan <pipio1994@gmail.com>
2016-05-06 15:40:23 +0800

    Reimplement filter/get with custom Iterable
    Before this commit, each time we call `Thread.filter()`, it will
    send request to get data immediately and don't cache.
    Since in the admin view there are many places calling the filter(),
    it costs a lot. Also, the `Post.get()` repeatedly calling the `Thread.get()`
    for each post getting from `get_posts_list`, even there's query condition
    (e.g primary key). That's a redundant behavior. Also, it's a bad idea to
    allow the `data` attribute changed in almost all the Post/Thread methods.
    
    So I think it's a better way to have a same design as Djnago ORM QuerySet:
    Use a Iterable class to get data from backstore. Only request data when necessary.
    So, now when calling `filter` it will just add the query condition and not really
    request the data.
    (but, because in the admin we only need to know the 'pk=XXX' query,
    so far we haven't(and don't need to) implement a full query parser.)
    Also, I try not to request thread data repeatedly in the PostIterable.
    
    Note this change will break the delete operation in admin. This should be fixed.

--- disqus_backstore/query.py
+++ disqus_backstore/query.py
@@ -1,116 +1,197 @@
+"""
+Most of the `disqus_backstore.query` module has
+the same interface in django `QuerySet` class,
+ex. the implementation of filter/get method.
+"""
+
 import copy
+
+from django.db.models.query import BaseIterable
 
 from .disqus_interface import DisqusQuery
 
 
+class Query(object):
+    def __init__(self, query_string, value, negate):
+        self.query_string = query_string
+        self.value = value
+        self.negate = negate
+
+
+def get_thread_from_thread_list(id, thread_list):
+    for thread in thread_list:
+        if thread.id == id:
+            return thread
+    return None
+
+def meet_querys(data, query_objs):
+    for q in query_objs:
+        if q.query_string == 'pk' or q.query_string == 'id':
+            if q.negate and data['id'] == str(q.value):
+                return False
+            elif not q.negate and data['id'] != str(q.value):
+                return False
+    return True
+
+
+class ThreadIterable(BaseIterable):
+    def __iter__(self):
+        queryset = self.queryset
+        #processing query string, currently only processing primary key
+        rawdata = queryset.query.get_threads_list()
+        for thread in rawdata['response']:
+            if meet_querys(thread, queryset.query_objs):
+                 obj = queryset.create(
+                     id=int(thread.get('id')),
+                     title=thread.get('title'),
+                     link=thread.get('link'),
+                     forum=thread.get('forum'),
+                     is_deleted=thread.get('isDeleted'),
+                     is_closed=thread.get('isClosed'),
+                 )
+        # Dirty hack for unique check in admin changeform view
+                 obj._state.adding = False
+                 yield obj
+
+
+class PostIterable(BaseIterable):
+    def __iter__(self):
+        queryset = self.queryset
+        #processing query string, currently only processing primary key
+        rawdata = queryset.query.get_posts_list()
+        #hack for reduce redundant thread list request
+        thread_field = queryset.model._meta.get_field('thread')
+        thread_list = list(thread_field.remote_field.model.objects.all())
+        for post in rawdata['response']:
+            if meet_querys(post, queryset.query_objs):
+                thread = get_thread_from_thread_list(post['thread'], thread_list)
+                obj = queryset.create(
+                    id=int(post.get('id')),
+                    forum=post.get('forum'),
+                    is_approved=post.get('isApproved'),
+                    is_deleted=post.get('isDeleted'),
+                    is_spam=post.get('isSpam'),
+                    message=post.get('raw_message'),
+                    thread=thread,
+                )
+                obj._state.adding = False
+                yield obj
+
+
 class DisqusQuerySet(object):
+
+    ##############
+    #Magic Method#
+    ##############
     def __init__(self, model=None, query=None, using=None, hints=None):
         self.model = model
         self.query = query or DisqusQuery()
         if using:
             self.using = using
-        self.data = []
         self._prefetch_related_lookups = []
+        self.query_objs = []
+        self._result_cache = None
 
+    def __iter__(self):
+        self._fetch_all()
+        return iter(self._result_cache)
+
+    def __getitem__(self, i):
+        if self._result_cache is None:
+            self._fetch_all()
+        return self._result_cache[i]
+
+    def __len__(self):
+        if self._result_cache is None:
+            self._fetch_all()
+        return len(self._result_cache)
+
+    # Private Method
+
+    def _fetch_all(self):
+        if self._result_cache is None:
+            self._result_cache = list(self.iterator())
+
+    def _clone(self, **kwargs):
+        clone = self.__class__(model=self.model, query=self.query, using=self.using)
+        clone.query_objs = self.query_objs
+        return clone
+
+    # Iterator factory
+    def iterator(self):
+        return self._iterable_class(self)
+
+    # Create Model Instance
     def create(self, *args, **kwargs):
         obj = self.model(**kwargs)
         return obj
 
-    def order_by(self, *args, **kwargs):
-        return self
-
-    def complex_filter(self, filter_obj):
-        return self
-
-    def all(self, *args, **kwargs):
-        return self._clone()
+    # Public Method which sends request
 
     def count(self, *args, **kwargs):
-        return len(self.data)
+        return len(self)
 
-    def iterator(self):
-        return iter(self.data)
+    def get(self, *args, **kwargs):
+        clone = self.filter(*args, **kwargs)
+        clone._fetch_all()
+        if len(clone._result_cache) == 1:
+            return clone._result_cache[0]
+        elif len(clone._result_cache) == 0:
+            return None
+        else:
+            raise clone.model.MultipleObjectsReturned(
+                "get() returned more than one %s -- it returned %s!" %
+                (self.model._meta.object_name, len(self._result_cache))
+            )
+
+    def exists(self):
+        if self._result_cache is None:
+            try:
+                self.iterator().next()
+            except StopIteration as e:
+                return False
+            return True
+        return bool(self._result_cache)
+
+    # Public method to add query, currently only support primary key
+
+    def filter(self, *args, **kwargs):
+        return self.filter_or_exclude(False, *args, **kwargs)
+
+    def exclude(self, *args, **kwargs):
+        return self.filter_or_exclude(True, *args, **kwargs)
+
+    def filter_or_exclude(self, negate, *args, **kwargs):
+        clone = self._clone()
+        for k, v in kwargs.items():
+            q = Query(k, v, negate=negate)
+            clone.query_objs.append(q)
+        return clone
+
+    # Dumb Implementation
 
     def using(self, alias):
         clone = self._clone()
         clone._db = alias
         return clone
 
-    def exists(self):
-        return bool(self.data)
+    def order_by(self, *args, **kwargs):
+        return self._clone()
 
-    def exclude(self, *args, **kwargs):
-        pk = kwargs.pop('pk', None)
-        if pk:
-            for d in self.data:
-                if d.id == pk:
-                    self.data.remove(d)
-        return self
+    def complex_filter(self, filter_obj):
+        return self._clone()
+
+    def all(self, *args, **kwargs):
+        return self._clone()
 
     def select_related(self, *fields):
-        obj = self._clone()
-        return obj
-
-    def __eq__(self, qs):
-        return set(self.data) == set(qs.data)
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, i):
-        return self.data[i]
-
-    def _clone(self, **kwargs):
-        clone = self.__class__(model=self.model, query=self.query, using=self.using)
-        clone.data = copy.deepcopy(self.data)
-        return clone
+        return self._clone()
 
 
 class ThreadQuerySet(DisqusQuerySet):
-    def get(self, *args, **kwargs):
-        kwargs['thread'] = kwargs.pop('id', None)
-        rawdata = self.query.get_threads_list(**kwargs)['response']
-        if len(rawdata) > 2:
-            raise self.model.MultipleObjectsReturned(
-                "get() returned more than one %s -- it returned %s!" %
-                (self.model._meta.object_name, len(rawdata))
-            )
-        elif len(rawdata) == 1:
-            thread = rawdata[0]
-            obj = self.create(
-                id=int(thread.get('id')),
-                title=thread.get('title'),
-                link=thread.get('link'),
-                forum=thread.get('forum'),
-                is_deleted=thread.get('isDeleted'),
-                is_closed=thread.get('isClosed'),
-            )
-            obj._state.adding = False
-            self.data.append(obj)
-            return obj
-
-    def filter(self, *args, **kwargs):
-        rawdata = self.query.get_threads_list()
-        self.data = [self.create(
-            id=int(thread.get('id')),
-            title=thread.get('title'),
-            link=thread.get('link'),
-            forum=thread.get('forum'),
-            is_deleted=thread.get('isDeleted'),
-            is_closed=thread.get('isClosed'),
-        ) for thread in rawdata['response']]
-        # Dirty hack for unique check in admin changeform view
-        for obj in self.data:
-            obj._state.adding = False
-        pk = kwargs.pop('id', None)
-        if pk:
-            for thread in self.data:
-                if thread.id == pk:
-                    self.data = [thread]
-                    break
-            if self.data[0].id != pk:
-                self.data = []
-        return self
+    def __init__(self, model=None, query=None, using=None, hints=None):
+        super(ThreadQuerySet, self).__init__(model, query, using, hints)
+        self._iterable_class = ThreadIterable
 
     def delete(self, obj):
         self.query.delete_thread(obj.id)
@@ -126,56 +207,9 @@ class ThreadQuerySet(DisqusQuerySet):
                 if old_val != new_val:
                     update_func = getattr(self.query, 'change_thread_'+attname)
                     result = update_func(new_instance.id, old_val, new_val)
-                    print result
+
 
 class PostQuerySet(DisqusQuerySet):
-    def get(self, *args, **kwargs):
-        """
-        Because there's no "post" argument in diqus fourm/listPost API
-        (But there's "thread" argument in forum/listThread API!),
-        we have to use posts/details API to get single post..
-        """
-        post_id = kwargs.pop('id')
-        post = self.query.get_post(post_id, **kwargs)['response']
-        thread_field = self.model._meta.get_field('thread')
-        thread = thread_field.remote_field.model.objects.get(id=post['thread'])
-        obj = self.create(
-            id=int(post.get('id')),
-            forum=post.get('forum'),
-            is_approved=post.get('isApproved'),
-            is_deleted=post.get('isDeleted'),
-            is_spam=post.get('isSpam'),
-            message=post.get('raw_message'),
-            thread=thread,
-        )
-        obj._state.adding = False
-        self.data.append(obj)
-        return obj
-
-    def filter(self, *args, **kwargs):
-        rawdata = self.query.get_posts_list()
-        for post in rawdata['response']:
-            thread_field = self.model._meta.get_field('thread')
-            thread = thread_field.remote_field.model.objects.get(id=post['thread'])
-            obj = self.create(
-                id=int(post.get('id')),
-                forum=post.get('forum'),
-                is_approved=post.get('isApproved'),
-                is_deleted=post.get('isDeleted'),
-                is_spam=post.get('isSpam'),
-                message=post.get('raw_message'),
-                thread=thread,
-            )
-            self.data.append(obj)
-        # Dirty hack for admin.changeform_view
-        for obj in self.data:
-            obj._state.adding = False
-        pk = kwargs.pop('id', None)
-        if pk:
-            for thread in self.data:
-                if thread.id == pk:
-                    self.data = [thread]
-                    break
-            if self.data[0].id != pk:
-                self.data = []
-        return self
+    def __init__(self, model=None, query=None, using=None, hints=None):
+        super(PostQuerySet, self).__init__(model, query, using, hints)
+        self._iterable_class = PostIterable
